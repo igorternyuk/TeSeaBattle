d81906b3ac8c857193be158a71ee2320d482f892
igorternyuk <xmonad100@gmail.com>
2017-05-15 17:38:36 +0300

    version-3.0

--- src/teseabattle/View.java
+++ src/teseabattle/View.java
@@ -31,24 +31,23 @@ public class View implements ModelListener{
     private final Dimension btnDim = new Dimension(120, 30);
     private final int WINDOW_WIDTH = 900, WINDOW_HEIGHT = 600;
     private final int FIELD_SIZE = 450;
-    private final int FIELD_DIMENSION = 10;
-    private final int CELL_SIZE = FIELD_SIZE / FIELD_DIMENSION;
+    private final int CELL_SIZE = FIELD_SIZE / Constants.FIELD_DIMENSION;
     private final int SHOT_DIAMETER = 16;
     private final int FONT_SIZE = 30;
     private final int SEPARATING_LINE_WIDTH = 5;
     private final Color DARK_GREEN = new Color(56, 107, 34);
     private final Font font;
-    Model model;
+    ConstantModel model; //View class can call only constant methods of the model
     Controller controller;
     JFrame window;
     Canvas canvas; //Renderer
     JPanel btnPanel;
     JButton btnNewGame, btnQuit;
     
-    public View(){
+    public View(ConstantModel model, Controller controller){
         this.font = new Font("Arial", Font.BOLD, FONT_SIZE);
-        model = new Model(FIELD_DIMENSION);
-        controller = new Controller(model);
+        this.model = model;
+        this.controller = controller;
         window = new JFrame(TITLE_OF_PROGRAM);
         window.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         window.setResizable(false);
@@ -64,8 +63,8 @@ public class View implements ModelListener{
                 int cursorX = e.getX() / CELL_SIZE;
                 int cursorY = e.getY() / CELL_SIZE;
                 boolean isPointInsideOfTheComputerField = cursorX >= 0 &&
-                        cursorX < FIELD_DIMENSION && cursorY >= 0 
-                        && cursorY < FIELD_DIMENSION;
+                        cursorX < Constants.FIELD_DIMENSION && cursorY >= 0 
+                        && cursorY < Constants.FIELD_DIMENSION;
                 if(isPointInsideOfTheComputerField && 
                    e.getButton() == MouseEvent.BUTTON1){
                     controller.shot(cursorX, cursorY);
@@ -103,10 +102,6 @@ public class View implements ModelListener{
         window.setVisible(true);
     }
     
-    public void go(){
-        model.addListener(this);
-    }
-    
     @Override
     public void updateView() {
         canvas.repaint();
@@ -117,12 +112,12 @@ public class View implements ModelListener{
         
         //Drawing of the field grid
         
-        for (int i = 0; i <= FIELD_DIMENSION; ++i) {
+        for (int i = 0; i <= Constants.FIELD_DIMENSION; ++i) {
             g.drawLine(0, i * CELL_SIZE, WINDOW_WIDTH, i * CELL_SIZE);
         }
 
-        for (int i = 0; i <= 2 * FIELD_DIMENSION; ++i) {
-            g.drawLine(i * CELL_SIZE, 0, i * CELL_SIZE, FIELD_DIMENSION * CELL_SIZE);
+        for (int i = 0; i <= 2 * Constants.FIELD_DIMENSION; ++i) {
+            g.drawLine(i * CELL_SIZE, 0, i * CELL_SIZE, Constants.FIELD_DIMENSION * CELL_SIZE);
         }
         
         //Drawing of the human's shots
@@ -142,7 +137,7 @@ public class View implements ModelListener{
         if (computerShotsNumber > 0) {
             for (int i = 0; i < computerShotsNumber; ++i) {
                 g.setColor(Color.YELLOW);
-                g.fillOval(FIELD_DIMENSION * CELL_SIZE + model.getComputerShotX(i)
+                g.fillOval(Constants.FIELD_DIMENSION * CELL_SIZE + model.getComputerShotX(i)
                         * CELL_SIZE + (CELL_SIZE - SHOT_DIAMETER) / 2 + 1,
                         model.getComputerShotY(i) * CELL_SIZE + (CELL_SIZE - SHOT_DIAMETER) / 2 + 1,
                         SHOT_DIAMETER, SHOT_DIAMETER);
@@ -157,7 +152,7 @@ public class View implements ModelListener{
                 g.setColor(model.isHumanShipCellAlive(i, j) ? Color.WHITE.darker() : Color.RED);
                 final int x = model.getHumanShipCell_x(i, j);
                 final int y = model.getHumanShipCell_y(i, j);
-                g.fill3DRect(FIELD_DIMENSION * CELL_SIZE + x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE, true);
+                g.fill3DRect(Constants.FIELD_DIMENSION * CELL_SIZE + x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE, true);
             }
         }
 
@@ -179,16 +174,16 @@ public class View implements ModelListener{
         
         g.setColor(DARK_GREEN);
         g.setFont(font);
-        g.drawString("Human ships: " + model.getHumanNavyHealth() + "%", 40, FIELD_DIMENSION * CELL_SIZE + 40);
-        g.drawString("Computer ships: " + model.getComputerNavyHealth() + "%", FIELD_DIMENSION * CELL_SIZE + 40, FIELD_DIMENSION * CELL_SIZE + 40);
+        g.drawString("Human ships: " + model.getHumanNavyHealth() + "%", 40, Constants.FIELD_DIMENSION * CELL_SIZE + 40);
+        g.drawString("Computer ships: " + model.getComputerNavyHealth() + "%", Constants.FIELD_DIMENSION * CELL_SIZE + 40, Constants.FIELD_DIMENSION * CELL_SIZE + 40);
         g.setColor(Color.BLUE);
-        g.drawString(model.getStatus(), WINDOW_WIDTH / 2 - 100, FIELD_DIMENSION * CELL_SIZE + 70);
+        g.drawString(model.getStatus(), WINDOW_WIDTH / 2 - 100, Constants.FIELD_DIMENSION * CELL_SIZE + 70);
 
         //Drawing the separating line
         
        g.setColor(DARK_GREEN);
        g.setStroke(new BasicStroke(SEPARATING_LINE_WIDTH));
-       g.drawLine(WINDOW_WIDTH / 2, 1, WINDOW_WIDTH / 2, FIELD_DIMENSION * CELL_SIZE - 2);
+       g.drawLine(WINDOW_WIDTH / 2, 1, WINDOW_WIDTH / 2, Constants.FIELD_DIMENSION * CELL_SIZE - 2);
        
     }
     
