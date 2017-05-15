5ab5abf908a66a77b42af4d63cd4fd6945e66ac3
igorternyuk <xmonad100@gmail.com>
2017-05-15 17:38:30 +0300

    version-3.0

--- src/teseabattle/TeSeaBattle.java
+++ src/teseabattle/TeSeaBattle.java
@@ -2,9 +2,16 @@ package teseabattle;
 
 public class TeSeaBattle {
 
-    public static void main(String[] args) {
-        View view = new View();
-        view.go();
+    public void go() {
+        Model model = new Model(Constants.FIELD_DIMENSION);
+        Controller controller = new Controller(model);
+        View view = new View(model, controller);
+        model.addListener(view);
     }
-    
+
+    public static void main(String[] args) {
+        TeSeaBattle battle = new TeSeaBattle();
+        battle.go();
+    }
+
 }
